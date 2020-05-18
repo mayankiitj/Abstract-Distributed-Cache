6df6292c17950eadb87f1bc444f4260a1031e0e1
Mayank Agrawal <Mayank.Agrawal@go-mmt.com>
2020-05-18 10:45:30 +0530

    skeleton project

---
+++ src/main/java/com/unacademy/cache/AbstractDistibutedCacheApplication.java
@@ -0,0 +1,13 @@
+package com.unacademy.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractDistibutedCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractDistibutedCacheApplication.class, args);
+	}
+
+}
